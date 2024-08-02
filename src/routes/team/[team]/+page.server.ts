fbf1bfa2a9a947b01e8f075802128a82575bde7c
Ludvig Hozman <ludvig123@gmail.com>
2024-08-02 09:17:27 +0200

    Remove console log

--- src/routes/team/[team]/+page.server.ts
+++ src/routes/team/[team]/+page.server.ts
@@ -3,6 +3,5 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = ({ params, cookies }) => {
 	const token = cookies.get('userToken');
-	console.log(token);
 	return getDeployments(params.team, token);
 };
